2d38a05eec307d401c6f26f19a05f213003d208a
marianacazzoto <MARIANA.CAZZOTO@bandtec.com.br>
2022-01-02 16:57:48 -0300

    Atualizações

--- operadoresaritimeticos/src/main/java/com/mycompany/operadoresaritimeticos/OperadoresAritmeticos.java
+++ operadoresaritimeticos/src/main/java/com/mycompany/operadoresaritimeticos/OperadoresAritmeticos.java
@@ -15,11 +15,21 @@ public class OperadoresAritmeticos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int n1 = 3;
+        /*int n1 = 3;
         int n2 = 5;
         float m = (n1 + n2)/2;
         
         System.out.println("Sua média é: " + m);
+        
+        int numero = 5;
+        int valor = 5 + ++numero;               
+        numero--;
+        System.out.println(valor);
+        System.out.println(numero);*/
+        
+        int x = 4;
+        x += 2;
+        System.out.println(x);
     }
     
 }
