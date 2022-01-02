0c9981a1614885f0da9734b7edb9a80936e83f0e
marianacazzoto <MARIANA.CAZZOTO@bandtec.com.br>
2022-01-02 17:38:33 -0300

    Atualizado

--- operadoresaritimeticos/src/main/java/com/mycompany/operadoresaritimeticos/OperadoresAritmeticos.java
+++ operadoresaritimeticos/src/main/java/com/mycompany/operadoresaritimeticos/OperadoresAritmeticos.java
@@ -14,22 +14,35 @@ public class OperadoresAritmeticos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here 
+        
         /*int n1 = 3;
         int n2 = 5;
         float m = (n1 + n2)/2;
+        System.out.println("Sua média é: " + m);*/
         
-        System.out.println("Sua média é: " + m);
-        
-        int numero = 5;
+        /*int numero = 5;
         int valor = 5 + ++numero;               
         numero--;
         System.out.println(valor);
         System.out.println(numero);*/
         
-        int x = 4;
-        x += 2;
-        System.out.println(x);
+        /*int x = 4;
+        x += 2; //x = x + 2
+        System.out.println(x);*/
+        
+        /*float n1 = 8.9f;
+        float n2 = 9.9f;
+        float n3 = 8.5f;
+        int arb = (int) Math.floor(n1); //Arredondamento para baixo
+        int arc = (int) Math.ceil(n2); //Arredomento para cima
+        int ara = (int) Math.round(n3); // Arredondamento aritmético
+        System.out.println(arb);
+        System.out.println(arc);
+        System.out.println(ara);*/
+        
+        double ale = 5 + Math.random()* (10-5);
+        System.out.println(ale);
     }
     
 }
