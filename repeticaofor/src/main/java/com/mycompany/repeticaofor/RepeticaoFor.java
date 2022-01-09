788900f82c62b4eabf00c16d3b43db6a4be1ebf1
marianacazzoto <MARIANA.CAZZOTO@bandtec.com.br>
2022-01-09 20:23:11 -0300

    Aula 13

---
+++ repeticaofor/src/main/java/com/mycompany/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.repeticaofor;
+
+/**
+ *
+ * @author Mariana Cazzoto
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int cc =1;cc<=3;cc++){
+            System.out.println(cc + " Cambalhota");
+        }
+    }
+    
+}
